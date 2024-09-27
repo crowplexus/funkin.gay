1275b5eecb1e8a701747a528f5d36202785e8e33
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-09-27 17:15:59 -0400

    Progress Bars (no images for now)

--- funkin/objects/hud/DefaultHUD.lua
+++ funkin/objects/hud/DefaultHUD.lua
@@ -22,45 +22,41 @@ end
 function DefaultHUD:new()
 	DefaultHUD.super.new()
 
-	self.scoreText = nil --- @type jigw.objects.Label
-	self.judgementCounter = nil --- @type jigw.objects.Label
+	self.scoreText = nil --- @class jigw.objects.Label
+	self.judgementCounter = nil --- @class jigw.objects.Label
 
 	local Label = require("jigw.objects.Label")
+  local ProgressBar = require("jigw.objects.ProgressBar")
   local vpw, vph = love.graphics.getDimensions()
 
-	self.scoreText = Label(0, 0, getScoreText(), 20)
-  self.scoreText.position.y = (vph - self.scoreText.size.y) - 15
-  self.scoreText:centerPosition("sex") -- funny how that works huh.
+  local downscroll = true
+  local healthBarY = downscroll and vpw * 0.05 or vpw * 0.5
+
+  self.healthBar = ProgressBar(0,healthBarY)
+  self.healthBar.colors = { Color.rgb(255,0,0), Color.rgb(102,255,51) }
+  self.healthBar.percentage = 50 -- center the primary rectangle in the bar
+  self.healthBar.fillMode = ProgressFill.RTL -- LTR, RTL, BTT, TTB
+  self.healthBar:centerPosition("x")
+
+	self.scoreText = Label(0, (healthBarY + self.healthBar:getHeight()) + 20, getScoreText(), 20)
+  --self.scoreText.position.y = (vph - self.scoreText.size.y) - 15
+  self.scoreText:centerPosition("x") -- funny how that works huh.
   self.scoreText.strokeSize = 1.25
-  --self:add(self.scoreText)
 
   self.judgementCounter = Label(5, 0, getJudges(), 20)
   self.judgementCounter.position.y = (vph - self.judgementCounter.size.y) * 0.5
   self.judgementCounter.strokeSize = 1.25
+
+  --self:add(self.healthBar)
+  --self:add(self.scoreText)
   --self:add(self.judgementCounter)
-
-  --[[ -- TODO
-  local downscroll = false
-  local hpb = downscroll and vpw * 0.175 or vpw * 0.875
-
-  self.healthBar = ProgressBar(0,hbp)
-  self.healthBar.fillDirection = ProgressFill.RTL -- LTR, RTL, BTT, TTB?
-  self.healthBar.colors = { Color.rgb(255,0,0), Color.rgb(102,255,51) }
-  self.healthBar.value = 50 -- center the primary rectangle in the bar
-  self.healthBar.centerPosition("X")]]
 end
 
 function DefaultHUD:draw()
 	-- TODO: better way to draw here??? idk
-	if self.scoreText and self.scoreText.draw then
-		self.scoreText:draw()
-	end
-	if self.judgementCounter and self.judgementCounter.draw then
-		self.judgementCounter:draw() -- TODO: better counter
-	end
-	--[[if self.healthBar and self.healthBar.draw then
-		self.healthBar:draw()
-	end]]
+	if self.healthBar and self.healthBar.draw then self.healthBar:draw() end
+	if self.scoreText and self.scoreText.draw then self.scoreText:draw() end
+	if self.judgementCounter and self.judgementCounter.draw then self.judgementCounter:draw() end
 end
 
 return DefaultHUD
---
+++ jigw/objects/ProgressBar.lua
@@ -0,0 +1,99 @@
+--- Contains fields to define which direction the bar foreground goes to
+--- @enum ProgressFill
+ProgressFill = {
+  LTR = 0, --- @type number Left to Right
+  RTL = 1, --- @type number Right to Left
+  TTB = 2, --- @type number Top to Bottom
+  BTT = 3, --- @type number Bottom to Top
+}
+
+--- Object used for a visual representation of a percentage.
+local ProgressBar = Object:extend() --- @class ProgressBar
+function ProgressBar:__tostring() return "ProgressBar" end
+
+function ProgressBar:new(x,y,w,h,colors)
+  if colors == nil or #colors < 2 then
+    colors = {Color.GREEN_YELLOW, Color.RED}
+  end
+  self.position   = Vector2(x,y)                    --- @class Vector2
+  self.size       = Vector2(w or 590,h or 10)       --- @class Vector2
+  self.colors     = colors                          --- @type table
+  self.rotation   = 0                               --- @type number
+  self.centered   = false                           --- @type boolean
+  self.visible    = true                            --- @type boolean
+  self.border     = {width=10,color=Color.BLACK}    --- @type table<number,table>
+  self.minPercent = 0                               --- @type number minimum percentage value, by default, set to 0
+  self.maxPercent = 100                             --- @type number maximum percentage value, by default, set to 100
+  self.percentage = 0                               --- @type number percentage of the progress bar, ranging from 0 to `self.maxValue`
+  self.fillMode   = ProgressFill.LTR                --- @type ProgressFill|number used to denominate where the progress color of the bar should go
+  return self
+end
+
+function ProgressBar:getWidth()
+  local more = self.border and self.border.width or 0
+  return self.size.x + more
+end
+
+function ProgressBar:getHeight() return self.size.y end
+
+function ProgressBar:draw()
+  if self.visible == true then
+    self:drawBackground()
+    self:drawProgress  ()
+  end
+end
+
+function ProgressBar:drawProgress()
+  love.graphics.push()
+  love.graphics.setColor(self.colors[2])
+
+  -- TODO: image filling?
+
+  local vertical = self.fillMode == ProgressFill.TTB or self.fillMode == ProgressFill.BTT
+
+  local perRange = self.maxPercent - self.minPercent
+  local rangeFrac = (self.percentage - self.minPercent) / perRange
+  local dependingSize = not vertical and self:getWidth() or self:getHeight()
+  local inReverse = not vertical and self.fillMode == ProgressFill.RTL or self.fillMode == ProgressFill.BTT
+
+  local fillSize = math.round(rangeFrac * dependingSize)
+  if inReverse then fillSize = dependingSize - fillSize end
+
+	love.graphics.translate(self.position:unpack())
+	love.graphics.rotate(self.rotation)
+  love.graphics.rectangle("fill",0,0,
+        vertical and self:getWidth() or fillSize,
+    not vertical and self:getHeight() or fillSize)
+  love.graphics.setColor(Color.WHITE)
+  love.graphics.pop()
+end
+
+function ProgressBar:drawBackground()
+  love.graphics.push()
+  love.graphics.translate(self.position:unpack())
+	love.graphics.rotate(self.rotation)
+  if(self.border and self.border.width > 0) then
+    love.graphics.setColor(self.border.color or Color.BLACK)
+    love.graphics.setLineWidth(self.border.width)
+    love.graphics.rectangle("line",0,0,self:getWidth(),self:getHeight())
+    love.graphics.setLineWidth(1)
+  end
+  love.graphics.setColor(self.colors[1])
+  love.graphics.rectangle("fill",0,0,self:getWidth(),self:getHeight())
+  love.graphics.setColor(Color.WHITE)
+  love.graphics.pop()
+end
+
+function ProgressBar:centerPosition(_x_)
+	if type(_x_) ~= "string" then _x_ = "xy" end
+	_x_ = string.lower(_x_)
+	local vpw, vph = love.graphics.getDimensions()
+	if string.find(_x_, "x") then
+		self.position.x = (vpw - self:getWidth()) * 0.5;
+	end
+	if string.find(_x_, "y") then
+		self.position.y = (vph - self:getWidth()) * 0.5;
+	end
+end
+
+return ProgressBar
