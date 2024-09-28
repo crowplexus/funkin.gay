840896456ef3661954653196af3bfcc270fe61c7
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-09-28 13:36:37 -0400

    modified:   funkin/screens/MainMenu.lua

--- funkin/screens/MainMenu.lua
+++ funkin/screens/MainMenu.lua
@@ -57,7 +57,7 @@ function MainMenu:enter()
   end
 
   local versionText = Label(5,0,"Funkin' Kiskadee v".._G.GAME_VER, 20)
-  versionText.position.y = (vph - versionText.size.y) - 5 -- i think that's the original pos idk
+  versionText.position.y = (vph - versionText.size.y) - 25 -- i think that's the original pos idk
   versionText.strokeSize = 1.5
   self:add(versionText)
 end
